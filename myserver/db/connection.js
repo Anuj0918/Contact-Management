564d17b3cb5088de375a8e5a8aa133a6a0cd0a63
Anuj <anujtiwari2822@gmail.com>
2024-11-16 11:39:17 +0530

    Done

--- myserver/db/connection.js
+++ myserver/db/connection.js
@@ -1,16 +1,5 @@
 
 const mongoose = require('mongoose');
-
-/*const conn = mongoose.connect(process.env.URL)
-        .then(db => {
-            console.log("Database Connected");
-            return db;
-        }).catch(err => {
-            console.log("Connection Error");
-        })
-
-module.exports = conn; */
-
 require('dotenv').config();
 
 const connectDB = async () => {
